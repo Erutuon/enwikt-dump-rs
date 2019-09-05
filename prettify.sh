9117b0def8b7483985937055497f382f4be12ef2
Erutuon <5840197+Erutuon@users.noreply.github.com>
2019-09-05 14:38:04 -0500

    cleaner

--- prettify.sh
+++ prettify.sh
@@ -1,2 +1,2 @@
-# for all_headers*
-sd '"counts": \[\s*(\d+),\s*(\d+),\s*(\d+),\s*(\d+),\s*(\d+),\s*(\d+)\s*\]' '"counts": [$1,$2,$3,$4,$5,$6]' $1
+# Makes pretty-printed JSON from all-headers subcommand more compact.
+sd '("counts":)\s*\[\s*(\d+),\s*(\d+),\s*(\d+),\s*(\d+),\s*(\d+),\s*(\d+)\s*\]' '$1 [$2,$3,$4,$5,$6,$7]' $1
