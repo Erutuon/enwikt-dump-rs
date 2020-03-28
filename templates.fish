d6fd64968bc9138650a7ed2c82ec264a3973f9cf
gbertilson <5840197+Erutuon@users.noreply.github.com>
2020-03-27 23:51:35 -0500

    improvements to Fish scripts

--- templates.fish
+++ templates.fish
@@ -2,16 +2,17 @@
 
 set -l template_names template_names.txt
 set -l template_names_cbor template_names_cbor.txt
-if test ! \( -f "$template_names_cbor" -a -s "$template_names_cbor" \)
-	cat $template_names | lua lua/add_template_redirects.lua "%s.cbor" template_redirects.json \
-		> $template_names_cbor
-	
-	cd cbor
-	echo "dumping parsed templates"
-	wiktionary-data dump-parsed-templates --input ../pages-articles.xml \
-		--templates ../$template_names_cbor \
-		--namespaces main,reconstruction,appendix \
-		--format cbor \
-		--include-text
-	cd ..
+cat $template_names | lua lua/add_template_redirects.lua "%s.cbor" template_redirects.json \
+	> $template_names_cbor
+
+if not begin; mkdir -p cbor; and cd cbor; end;
+	echo "Failed to create output directory"; exit -1
 end
+echo "dumping parsed templates"
+wiktionary-data dump-parsed-templates \
+	--input ../pages-articles.xml \
+	--templates ../$template_names_cbor \
+	--namespaces main,reconstruction,appendix \
+	--format cbor \
+	--include-text
+cd ..
